71280d849faf2edd6770f4566b29d2a6ffd8d185
Gedler Jean Pierre <gedler.jeanpierre@stonybrook.edu>
2021-07-05 16:36:29 -0400

    Basic Fetches & Login Working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from "./fanpages/Login"
+//import { useHistory } from 'react-router-dom';
+import React, { Component } from 'react'
+//import {Route, Switch, Link} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  
+ 
+  state = {
+
+    currentFan: "", 
+    all_fans: [],
+    concerts: [], 
+    all_artists: {},
+    attending_concerts: [],
+    followers: [],
+    token: false, 
+
+  }
+
+  componentDidMount(){
+
+    fetch("http://localhost:3001/concerts")
+    .then(res => res.json())
+    .then(concerts => {
+      this.setState({
+        concerts: concerts
+      })
+  })
+
+  fetch("http://localhost:3001/fans")
+  .then(res => res.json())
+  .then(all_fans => {
+    this.setState({
+      all_fans: all_fans
+    })
+  })
+
+
+  fetch("http://localhost:3001/artists")
+  .then(res => res.json())
+  .then(artists => {
+    this.setState({
+      all_artists: artists
+    })
+
+  })
+
+  fetch("http://localhost:3001/attending_concerts")
+  .then(res => res.json())
+  .then(attending_concerts => {
+    this.setState({
+      attending_concerts: attending_concerts
+    })
+  })
+
+  fetch("http://localhost:3001/followers")
+  .then(res => res.json())
+  .then(followers => {
+    this.setState({
+      followers: followers})
+  })
+
+  this.finishLogin = (userInfo) => {
+    this.setState({
+      currentFan: userInfo.fan,
+      token: !this.state.token
+      
+    })
+     console.log(userInfo)
+      this.localToken(userInfo)
+  
+  }
+  
+  this.localToken=(obj)=>{
+  console.log("This is the localToken",obj)
+  console.log(obj.fan)
+
+    if (localStorage.token !== "undefined" && localStorage.length === 1){
+      this.setState({
+        fanName: obj.fan
+      })
+    }
+  }
 }
 
-export default App;
+
+  
+ 
+
+
+
+
+
+
+  
+
+
+
+
+  
+
+  render() {
+
+  
+  return (
+    <div className="App">
+      <Login finishLogin={this.finishLogin}>
+             token={this.state.token}
+             loggedInFan={this.state.currentFan}
+
+      </Login>
+    </div>
+  )
+}
+}
+
+
+
+
---
+++ src/fanpages/Login.js
@@ -0,0 +1,61 @@
+//import { useHistory } from "react-router";
+//import { Link } from "react-router-dom";
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+    componentDidMount(){
+
+
+
+this.handleLogin=(e)=> {
+    e.preventDefault()
+
+    fetch("http://localhost:3001/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+                },
+        body: JSON.stringify({
+            user: e.target[0].value,
+            password: e.target[1].value
+        })
+    })
+    
+    .then(res => res.json())
+    .then((userInfo) => {
+          console.log("This is the 2nd .then",userInfo)
+    localStorage.token = userInfo.token
+    this.props.finishLogin(userInfo)
+
+    })
+}
+}
+render() {
+return (
+    <div>
+        <h1>Concert Hunt</h1>
+        <form onSubmit={(e)=> this.handleLogin(e)}>
+            <label>Username</label>
+            <input name="username" type="text"/>
+            <label>Password</label>
+            <input name="password" type="text"/>
+            <input type="submit"/>
+        </form>
+        </div>
+    
+
+
+)
+
+}
+}
+
+
+
+
+
+
+
+
+
